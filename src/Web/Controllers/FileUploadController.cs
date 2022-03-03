7246bacc6c6f90c7903a81b7a25d87083e1d82eb
hiroyay-ms <hiroyay@microsoft.com>
2022-03-03 16:23:06 +0900

    Add and Remove dotnet package

--- src/Web/Controllers/FileUploadController.cs
+++ src/Web/Controllers/FileUploadController.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Storage;
-using Microsoft.Azure.Storage.Blob;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
 
 namespace Web.Controllers
 {
@@ -25,54 +27,56 @@ namespace Web.Controllers
         public async Task<IActionResult> SaveImages(List<IFormFile> inputFiles)
         {
             var connectionString = _configuration.GetValue<string>("BlobConnectionString");
+            var accountName = _configuration.GetValue<string>("StorageAccountName");
             var containerName = _configuration.GetValue<string>("ContainerName");
 
             List<string> files = new List<string>();
 
-            CloudStorageAccount storageAccount;
-            if (CloudStorageAccount.TryParse(connectionString, out storageAccount))
+            BlobContainerClient containerClient;
+
+            if (string.IsNullOrEmpty(connectionString))
             {
-                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-                CloudBlobContainer blobContainer = blobClient.GetContainerReference(containerName);
+                var containerEndpoint = string.Format("https://{0}.blob.core.windows.net/{1}", accountName, containerName);
 
-                long size = inputFiles.Sum(f => f.Length);
-
-                foreach(var file in inputFiles)
-                {
-                    if (file.Length > 0)
-                    {
-                        var filePath = Path.GetTempFileName();
-                        var memoryStream = new MemoryStream();
-
-                        using (var stream = new FileStream(filePath, FileMode.Create))
-                            file.CopyTo(stream);
-
-                        using (var fileStream = new FileStream(filePath, FileMode.Open))
-                            fileStream.CopyTo(memoryStream);
-
-                        memoryStream.Position = 0;
-
-                        CloudBlockBlob blockBlob = blobContainer.GetBlockBlobReference(file.FileName);
-                        var requestOption = new BlobRequestOptions()
-                        {
-                            ParallelOperationThreadCount = 4
-                        };
-
-                        await blockBlob.UploadFromStreamAsync(memoryStream, AccessCondition.GenerateIfNotExistsCondition(), requestOption, null);
-
-                        blockBlob.Properties.ContentType = file.ContentType;
-
-                        await blockBlob.SetPropertiesAsync();
-
-                        files.Add($"{file.FileName} (ContentType: {file.ContentType}) -{file.Length} bytes");
-                    }
-                }
+                containerClient = new BlobContainerClient(new Uri(containerEndpoint), new DefaultAzureCredential());
             }
             else
             {
-                _logger.LogInformation($"A connection string has not been defined in the system environment variables.");
+                containerClient = new BlobContainerClient(connectionString, containerName);
+            }
 
-                throw new Exception("A connection string has not been defined in the system environment variables.");
+            await containerClient.CreateIfNotExistsAsync();
+
+            foreach (var file in inputFiles)
+            {
+                if (file.Length > 0)
+                {
+                    BlobClient blobClient = containerClient.GetBlobClient(file.FileName);
+
+                    var filePath = Path.GetTempFileName();
+                    var memoryStream = new MemoryStream();
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                        file.CopyTo(stream);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Open))
+                        fileStream.CopyTo(memoryStream);
+                    
+                    memoryStream.Position = 0;
+
+                    await blobClient.UploadAsync(memoryStream, overwrite: false);
+
+                    BlobProperties properties = await blobClient.GetPropertiesAsync();
+
+                    BlobHttpHeaders headers = new BlobHttpHeaders
+                    {
+                        ContentType = file.ContentType
+                    };
+
+                    await blobClient.SetHttpHeadersAsync(headers);
+
+                    files.Add($"{file.FileName} (ContentType: {file.ContentType} - {file.Length} bytes)");
+                }
             }
 
             ViewBag.Files = files;
